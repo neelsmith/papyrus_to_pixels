fe47711bb277517392a9b63e54d15da8cc131284
Neel Smith <dnsmith.neel@gmail.com>
2024-01-20 06:59:09 -0500

    Add material for lab 2

---
+++ pluto/dna-strings.jl
@@ -0,0 +1,11 @@
+### A Pluto.jl notebook ###
+# v0.19.36
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 08162444-b78b-11ee-3077-812550ee5ff9
+md"""# Reading strings: DNA and texts"""
+
+# ╔═╡ Cell order:
+# ╟─08162444-b78b-11ee-3077-812550ee5ff9
